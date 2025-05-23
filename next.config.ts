5909b3c49a84647da417860ac7918717a6432b9d
JianAn Chen <john.data.chen@gmail.com>
2025-05-23 10:55:16 +0800

    build: initialize project with essential configs and dependencies

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  compiler: {
+    relay: {
+      src: './',
+      artifactDirectory: './__generated__',
+      language: 'typescript',
+      eagerEsModules: false
+    }
+  }
+};
+
+export default nextConfig;
